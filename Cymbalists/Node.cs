0be9ac01781532799ae50795eefde6c84e140297
sowiks2711 <sowiks@student.mini.pw.edu.pl>
2018-05-04 18:48:56 +0200

    Add Node class

---
+++ Cymbalists/Node.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
+namespace Cymbalists
+{
+    public class Node
+    {
+
+        public Node(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Neighbours = new List<Node>();
+        }
+
+        public int X { get; }
+        public int Y { get; }
+        public List<Node> Neighbours { get; }
+
+    }
+}
